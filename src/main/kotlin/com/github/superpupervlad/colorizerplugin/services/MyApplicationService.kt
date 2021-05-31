32efa8f981f43b0a68224ff62730c4148f7cabc2
GitHub Action <action@github.com>
2021-05-31 10:40:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/superpupervlad/colorizerplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.superpupervlad.colorizerplugin.services
+
+import com.github.superpupervlad.colorizerplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/superpupervlad/colorizerplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/superpupervlad/colorizerplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.superpupervlad.colorizerplugin.services
 
+import com.github.superpupervlad.colorizerplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
